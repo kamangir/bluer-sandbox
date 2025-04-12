7728544ad56e86443aeb14ea3b5a11713b81e07c
kamangir <arash@kamangir.net>
2025-04-12 11:32:15 -0700

    start of refactors-2025-04-12-14wihk 🌀 - kamangir/bolt#746

--- bluer_sandbox/help/functions.py
+++ bluer_sandbox/help/functions.py
@@ -7,28 +7,10 @@ from bluer_sandbox.help.notebooks import help_functions as help_notebooks
 from bluer_sandbox import ALIAS
 
 
-def help_browse(
-    tokens: List[str],
-    mono: bool,
-) -> str:
-    options = "actions|repo"
-
-    return show_usage(
-        [
-            "@sandbox",
-            "browse",
-            f"[{options}]",
-        ],
-        "browse bluer_sandbox.",
-        mono=mono,
-    )
-
-
 help_functions = generic_help_functions(plugin_name=ALIAS)
 
 help_functions.update(
     {
-        "browse": help_browse,
         "notebooks": help_notebooks,
     }
 )
