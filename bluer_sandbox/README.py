952e95be80135e093119663b2dfeaca45fc9b59a
kamangir <arash@kamangir.net>
2025-04-19 15:21:21 -0700

    ++ - kamangir/bolt#746

--- bluer_sandbox/README.py
+++ bluer_sandbox/README.py
@@ -22,6 +22,7 @@ items = README.Items(
         {
             "name": "offline LLM",
             "description": "using [llama.cpp](https://github.com/ggerganov/llama.cpp).",
+            "marquee": "https://user-images.githubusercontent.com/1991296/230134379-7181e485-c521-4d23-a0d6-f7b3b61ba524.png",
             "url": "./bluer_sandbox/docs/offline_llm.md",
         },
     ]
