76c7a94047b85c4dde99bccfc91f7a9b614e3227
kamangir <arash@kamangir.net>
2025-05-23 13:28:33 +0330

    ++ - kamangir/bolt#746

--- bluer_sandbox/.abcli/arvancloud/ssh.sh
+++ bluer_sandbox/.abcli/arvancloud/ssh.sh
@@ -4,7 +4,12 @@ function bluer_sandbox_arvancloud_ssh() {
     local options=$1
     local do_dryrun=$(bluer_ai_option_int "$options" dryrun 0)
 
-    local pem_filename="~/.ssh/$ARVANCLOUD_PRIVATE_KEY.pem"
+    local pem_filename="$HOME/.ssh/$ARVANCLOUD_PRIVATE_KEY.pem"
+    if [[ ! -f "$pem_filename" ]]; then
+        bluer_ai_log_error "$pem_filename not found."
+        return 1
+    fi
+
     chmod 400 $pem_filename
 
     bluer_ai_eval dryrun=$do_dryrun \
