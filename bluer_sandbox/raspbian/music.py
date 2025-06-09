6f3a73752963f9a01b94d3d041e68b813f27641c
kamangir <arash@kamangir.net>
2025-06-09 16:39:49 +0330

    ++ - kamangir/bolt#746

--- bluer_sandbox/raspbian/music.py
+++ bluer_sandbox/raspbian/music.py
@@ -17,5 +17,62 @@ def beep(frequency=440, duration_ms=500):
     pygame.mixer.quit()
 
 
+note_frequencies = {
+    "C4": 261.63,
+    "C#4": 277.18,
+    "Db4": 277.18,
+    "D4": 293.66,
+    "D#4": 311.13,
+    "Eb4": 311.13,
+    "E4": 329.63,
+    "F4": 349.23,
+    "F#4": 369.99,
+    "Gb4": 369.99,
+    "G4": 392.00,
+    "G#4": 415.30,
+    "Ab4": 415.30,
+    "A4": 440.00,
+    "A#4": 466.16,
+    "Bb4": 466.16,
+    "B4": 493.88,
+    "C5": 523.25,
+}
+
+pirates_notes = [
+    "E4",
+    "G4",
+    "A4",
+    "A4",  # "Da da da da"
+    "E4",
+    "G4",
+    "B4",
+    "B4",  # "Da da da da"
+    "E4",
+    "G4",
+    "A4",
+    "A4",  # repeat pattern
+    "G4",
+    "B4",
+    "A4",  # descending part
+    "G4",
+    "E4",
+    "G4",
+    "A4",  # leading up again
+    "B4",
+    "C5",
+    "D5",
+    "D5",  # climax
+    "E5",
+    "D5",
+    "C5",
+    "B4",  # descending
+    "A4",
+    "A4",  # ending phrase
+]
+
 # Example
-beep(440, 500)
+# beep(440, 500)
+
+for note in pirates_notes:
+    freq = note_frequencies.get(note, 440)  # default to A4 if not found
+    beep(freq, 300)  # play each note for 300 ms
