a4dba0c5ea93f9d0dd299d70a207b44c32bd7797
kamangir <arash@kamangir.net>
2025-07-20 13:00:48 +0330

    ++ - kamangir/bolt#746

--- bluer_sandbox/README.py
+++ bluer_sandbox/README.py
@@ -16,6 +16,12 @@ items = README.Items(
             "url": "./bluer_sandbox/docs/arvancloud.md",
         },
         {
+            "name": "tor",
+            "description": "tools to work with [tor](https://www.torproject.org/).",
+            "marquee": "https://github.com/kamangir/assets/blob/main/tor/tor2.png?raw=true",
+            "url": "./bluer_sandbox/docs/alias/tor.md",
+        },
+        {
             "name": "offline LLM",
             "description": "using [llama.cpp](https://github.com/ggerganov/llama.cpp).",
             "marquee": "https://user-images.githubusercontent.com/1991296/230134379-7181e485-c521-4d23-a0d6-f7b3b61ba524.png",
