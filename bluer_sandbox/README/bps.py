97428202e6d31f3d7a5767d7014aea15d20985a4
kamangir <arash@kamangir.net>
2025-10-09 16:09:21 +0330

    pipeline fix, hopefully - kamangir/bolt#746

--- bluer_sandbox/README/bps.py
+++ bluer_sandbox/README/bps.py
@@ -4,7 +4,7 @@ from bluer_sandbox.README.consts import bps_assets2
 
 docs = [
     {
-        "path": f"../docs/bps.md",
+        "path": "../docs/bps.md",
         "items": ImageItems(
             {f"{bps_assets2}/{index:02}.png": "" for index in range(3)}
         ),
