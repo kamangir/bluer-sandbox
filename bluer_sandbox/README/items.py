d02fa1b9b12930e5b2ecff35d794cdb20b92ae54
kamangir <arash@kamangir.net>
2025-10-09 11:11:55 +0330

    ++ - kamangir/bolt#746 - no-workflow 🪄

--- bluer_sandbox/README/items.py
+++ bluer_sandbox/README/items.py
@@ -1,12 +1,12 @@
 from bluer_objects import README
-from bluer_objects.README.consts import assets
+from bluer_objects.README.consts import assets, assets2
 
 items = README.Items(
     [
         {
             "name": "ai4k",
             "description": "ai for kids.",
-            "marquee": f"{assets}/ai4k/20250604_154200.jpg?raw=true",
+            "marquee": f"{assets2}/ai4k/20250604_154200.jpg?raw=true",
             "url": "./bluer_sandbox/docs/ai4k",
         },
         {
