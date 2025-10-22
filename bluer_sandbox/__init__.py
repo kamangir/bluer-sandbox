f0929d8e30cd9a2028a7a60aee7fec48a9298664
kamangir <arash@kamangir.net>
2025-10-22 12:12:16 +0330

    ++ - kamangir/bolt#746

--- bluer_sandbox/__init__.py
+++ bluer_sandbox/__init__.py
@@ -4,7 +4,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} A sandbox for ideas and experiments."
 
-VERSION = "5.318.1"
+VERSION = "5.319.1"
 
 REPO_NAME = "bluer-sandbox"
 
--- sandbox/bps/v1/beacon.py
+++ sandbox/bps/v1/beacon.py
@@ -6,9 +6,11 @@ adv = advertisement.Advertisement(1, ble_adapter.address)
 adv.local_name = "TEST-PI"
 adv.manufacturer_data = {0xFFFF: struct.pack("<fff", 1.0, 2.0, 3.0)}
 adv.start()
-print("Advertising… (Ctrl+C to stop)")
+
+print("advertisement loop started ...")
 try:
     while True:
         time.sleep(1)
+        print("advertising ... (Ctrl+C to stop)")
 except KeyboardInterrupt:
     adv.stop()
