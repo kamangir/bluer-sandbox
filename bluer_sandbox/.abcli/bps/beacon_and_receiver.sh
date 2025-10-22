3beb55c96485f07a3228d8df6ef34ff9a2d498b8
kamangir <arash@kamangir.net>
2025-10-22 20:33:04 +0330

    ++ - kamangir/bolt#746

--- bluer_sandbox/.abcli/bps/beacon_and_receiver.sh
+++ bluer_sandbox/.abcli/bps/beacon_and_receiver.sh
@@ -3,6 +3,13 @@
 function bluer_sandbox_bps_beacon_and_receiver() {
     sudo systemctl restart bluetooth
 
+    sudo btmgmt le on
+    sudo btmgmt power off
+    sudo btmgmt power on
+    sudo btmgmt advertising on
+
+    sudo btmgmt info
+
     bluer_ai_eval ,$1 \
         python3 -m \
         bluer_sandbox.bps.utils.beacon_and_receiver \
