7b7d360e8f3b1717f7a4196ae05224907ecc271d
kamangir <arash@kamangir.net>
2025-10-22 17:49:22 +0330

    ++ - kamangir/bolt#746

--- bluer_sandbox/.abcli/bps/test.sh
+++ bluer_sandbox/.abcli/bps/test.sh
@@ -4,6 +4,6 @@ function bluer_sandbox_bps_test() {
     bluer_ai_eval ,$1 \
         sudo -E \
         $(which python3) -m \
-        bluer_sandbox.bps.test \
+        bluer_sandbox.utils.bps.test \
         "${@:2}"
 }
--- bluer_sandbox/__init__.py
+++ bluer_sandbox/__init__.py
@@ -4,7 +4,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} A sandbox for ideas and experiments."
 
-VERSION = "5.379.1"
+VERSION = "5.380.1"
 
 REPO_NAME = "bluer-sandbox"
 
