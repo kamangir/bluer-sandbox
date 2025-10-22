638399605939f4ef208fcb4436ce8af164fd4d40
kamangir <arash@kamangir.net>
2025-10-22 13:07:45 +0330

    ++ - kamangir/bolt#746

--- sandbox/bps/v1/bps.sh
+++ sandbox/bps/v1/bps.sh
@@ -30,61 +30,24 @@ function runme() {
         pip install bleak
 
         # ----
-        local bluez_version=5.75
-        local tar_filename=bluez-$bluez_version.tar.xz
-        local dir_name=bluez-$bluez_version
-        bluer_ai_log "upgrading bluez to $bluez_version..."
-
-        pushd $abcli_path_git >/dev/null
-
-        sudo apt remove --purge bluez -y
-        sudo apt update
-        sudo apt install -y \
-            build-essential \
-            libdbus-1-dev \
-            libglib2.0-dev \
-            libudev-dev \
-            libical-dev \
-            libreadline-dev
-
-        if [[ ! -d "$dir_name" ]]; then
-            [[ ! -f "$tar_filename" ]] &&
-                wget https://www.kernel.org/pub/linux/bluetooth/$tar_filename
-
-            tar xf $tar_filename
-        fi
-
-        cd $dir_name
-        ./configure --enable-experimental
-        make -j4
-        sudo make install
-        sudo systemctl restart bluetooth
-
-        bluetoothctl --version
-
-        popd $abcli_path_git >/dev/null
+        bluer_ai_log "installing bluez ..."
+        sudo apt install --reinstall bluez -y
 
         # --experimental
         bluer_ai_log "turning experimental on ..."
 
-        local service_name=/usr/local/libexec/bluetooth/bluetoothd
+        sudo mkdir -pv /etc/systemd/system/bluetooth.service.d
 
-        bluer_objects_file \
-            sudo \
-            replace \
-            /lib/systemd/system/bluetooth.service \
-            --this "ExecStart=$service_name" \
-            --that "ExecStart=$service_name --experimental" \
-            --cat $verbose \
-            --save 1 \
-            --whole_line 1
+        mv -v \
+            ./override.conf \
+            /etc/systemd/system/bluetooth.service.d/override.conf >/dev/null
 
         sudo systemctl daemon-reexec
         sudo systemctl daemon-reload
+
         sudo systemctl restart bluetooth
 
-        bluer_ai_log "expecting: $service_name --experimental"
-        ps aux | grep bluetoothd
+        ps -eo args | grep [b]luetoothd
     fi
 
     if [[ "$do_start" == 1 ]]; then
