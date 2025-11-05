2aa88aa002c3dd78b4d811c5b8d874f07c80d480
kamangir <arash@kamangir.net>
2025-11-05 12:17:47 +0330

    ++ - kamangir/bolt#746

--- bluer_sandbox/README/items.py
+++ bluer_sandbox/README/items.py
@@ -1,12 +1,13 @@
 from bluer_objects import README
-from bluer_objects.README.consts import assets
+from bluer_objects.README.consts import assets, assets2
 
 items = README.Items(
     [
         {
-            "name": "bluer village",
-            "description": "a bluer village.",
-            "url": "./bluer_sandbox/docs/aliases/village.md",
+            "name": "v2ray",
+            "description": "tools to work with [v2ray](hhttps://en.wikipedia.org/wiki/V2Ray).",
+            "marquee": f"{assets2}/v2ray/v2ray.png?raw=true",
+            "url": "./bluer_sandbox/docs/v2ray.md",
         },
         {
             "name": "arvancloud",
@@ -25,6 +26,11 @@ items = README.Items(
             "description": "using [llama.cpp](https://github.com/ggerganov/llama.cpp).",
             "marquee": "https://user-images.githubusercontent.com/1991296/230134379-7181e485-c521-4d23-a0d6-f7b3b61ba524.png",
             "url": "./bluer_sandbox/docs/offline_llm.md",
+        },
+        {
+            "name": "bluer village",
+            "description": "a bluer village.",
+            "url": "./bluer_sandbox/docs/aliases/village.md",
         },
     ]
 )
