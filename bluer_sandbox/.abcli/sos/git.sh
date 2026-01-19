bd4a2c01aa4e4cc8dffc04e735b5d23816deca24
kamangir <arash@kamangir.net>
2026-01-20 00:00:42 +0330

    ++

--- bluer_sandbox/.abcli/sos/git.sh
+++ bluer_sandbox/.abcli/sos/git.sh
@@ -3,20 +3,34 @@
 function bluer_sandbox_sos_git() {
     local options=$1
     local do_dryrun=$(bluer_ai_option_int "$options" dryrun 0)
-
     local repo_name=$(bluer_ai_option "$options" repo)
 
-    local source=$abcli_path_git
-    local destination=/Volumes/kamangir/git
     if [[ ! -z "$repo_name" ]]; then
-        source=$source/$repo_name
-        destination=$destination/$repo_name
+        local source=$abcli_path_git/$repo_name
+        local destination=/Volumes/kamangir/git/$repo_name
+
+        bluer_ai_eval dryrun=$do_dryrun \
+            rm -rfv $destination
+
+        bluer_ai_eval dryrun=$do_dryrun \
+            cp -Rv \
+            $source \
+            $destination
+
+        return
     fi
 
-    rm -rfv $destination
+    pushd $abcli_path_git >/dev/null
 
-    bluer_ai_eval dryrun=$do_dryrun \
-        cp -Rv \
-        $source \
-        $destination
+    for repo_name in $(ls -d */); do
+        if [[ ",assets/,assets2/," == *",$repo_name,"* ]]; then
+            bluer_ai_log "skipping $repo_name"
+            continue
+        fi
+
+        bluer_sandbox_sos_git repo=$repo_name,$options
+    done
+
+    popd >/dev/null
+
 }
