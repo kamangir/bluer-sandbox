86421a009e6e9a9422d6b7752efc7b00b98a0de2
kamangir <arash@kamangir.net>
2026-01-28 00:00:30 +0330

    hot fix

--- bluer_sandbox/.abcli/tests/speedtest.sh
+++ bluer_sandbox/.abcli/tests/speedtest.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
 function test_bluer_sandbox_speedtest() {
-    if [[ "$BLUER_AI_INTERNET_OUTSIDE_IS_ACCESSIBLE" == 0 ]]; then
-        bluer_ai_log_warning "outside is not accessible, test is disabled."
+    if [[ "$BLUER_AI_WEB_IS_ACCESSIBLE" == 0 ]]; then
+        bluer_ai_log_warning "web is not accessible, test is disabled."
         return
     fi
 
